1d36a925afe9116dab443fb105c8220e109dd8eb
Greg Mercer <gmercer@stanford.edu>
2016-03-25 14:43:29 -0700

    WP-6765 Removing the previous code which cleared either instance date or instance text.

--- js/gsb_feature_program_instance_ct_edit.js
+++ js/gsb_feature_program_instance_ct_edit.js
@@ -6,24 +6,8 @@
 Drupal.behaviors.gsbProgramInstanceEdit = {
   attach: function (context) {
 
-    $(context).find('input[name="field_instance_date_text_select[und]"]:radio').once(function () {
-      var $element = $(this);
-      $element.change(function () {
-        $checked = $('input[type="radio"][name="field_instance_date_text_select[und]"]:checked');
-        // Clear the Text input field if the 'Date' radio is checked
-        if ($checked.val() == 'Date') {
-          var instanceText = $.find('input[name="field_instance_text[und][0][value]"]')[0];
-          $(instanceText).val('');
-        }
-        // Clear the Start and End Date input fields if the 'Text' radio is checked
-        if ($checked.val() == 'Text') {
-          var dateText1 = $.find('input[name="field_instance_date[und][0][value][date]"]')[0];
-          var dateText2 = $.find('input[name="field_instance_date[und][0][value2][date]"]')[0];
-          $(dateText1).val('');
-          $(dateText2).val('');
-        }
-      });
-    });
+  // We now handle changes to Instance Date and Instance Text
+  // in the form submit.
 
   }
 };
