cccd042858827aa617e308d9b25779d6ff91ca52
Ayuta Padhi <apadhi@stanford.edu>
2017-02-01 12:39:45 -0800

    WP-7743 Added Application State

--- js/gsb_feature_program_instance_ct_edit.js
+++ js/gsb_feature_program_instance_ct_edit.js
@@ -6,7 +6,7 @@
 Drupal.behaviors.gsbProgramInstanceEdit = {
   attach: function (context) {
 
-  // We now handle changes to Instance Date and Instance Text
+   // We now handle changes to Instance Date and Instance Text
   // in the form submit.
 
   }
